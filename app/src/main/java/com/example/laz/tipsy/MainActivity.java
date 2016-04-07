b8a79996bc49bb4824247676e5a7f07f2708e404
Mykell Mcswain <mykellmcswain@gmail.com>
2016-04-07 00:34:08 -0400

    "DATABASE INSERT"

--- app/src/main/java/com/example/laz/tipsy/MainActivity.java
+++ app/src/main/java/com/example/laz/tipsy/MainActivity.java
@@ -1,52 +1,58 @@
 package com.example.laz.tipsy;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText brand, name, region, vintage, producer, type,
+            aroma, taste, calories, volume, temperature;
+    WineDatabaseAdapter wineHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        brand=(EditText) findViewById(R.id.brandValue);
+        name=(EditText) findViewById(R.id.nameValue);
+        region=(EditText) findViewById(R.id.regionValue);
+        vintage=(EditText) findViewById(R.id.vintageValue);
+        producer=(EditText) findViewById(R.id.producerValue);
+        type=(EditText) findViewById(R.id.typeValue);
+        aroma=(EditText) findViewById(R.id.aromaValue);
+        taste=(EditText) findViewById(R.id.tasteValue);
+        calories=(EditText) findViewById(R.id.caloriesValue);
+        volume=(EditText) findViewById(R.id.volumeValue);
+        temperature=(EditText) findViewById(R.id.temperatureValue);
+
+        wineHelper=new WineDatabaseAdapter(this);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void addWine(View view) {
+        String Brand = brand.getText().toString();
+        String Name = name.getText().toString();
+        String Region = region.getText().toString();
+        String Vintage = vintage.getText().toString();
+        String Producer = producer.getText().toString();
+        String Type = type.getText().toString();
+        String Aroma = aroma.getText().toString();
+        String Taste = taste.getText().toString();
+        String Calories = calories.getText().toString();
+        String Volume = volume.getText().toString();
+        String Temperature = temperature.getText().toString();
+
+        long id = wineHelper.insertData(Brand, Name, Region, Vintage, Producer, Type, Aroma,
+                Taste, Calories, Volume, Temperature);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
